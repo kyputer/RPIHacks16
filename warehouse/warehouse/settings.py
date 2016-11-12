a5eb6e9e9bb49e5db2a6d2087d3ab46ddcf8bf69
Orens Xhagolli <orens.xhagolli@hotmail.com>
2016-11-12 14:36:57 -0500

    Set up template routing

--- warehouse/warehouse/settings.py
+++ warehouse/warehouse/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'warehouse.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            'templates/html'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- warehouse/whtech/views.py
+++ warehouse/whtech/views.py
@@ -1,23 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template.loader import get_template
 
 # Create your views here.
 
 def login(request):
-    return HttpResponse(request.method)
+    return render(request, "login.html")
 
 
 def logout(request):
-    return HttpResponse(request.method)
+    #logout
+    return render(request, "index.html")
 
 
 def index(request):
-    return HttpResponse("Hello")
+    return render(request, "index.html")
 
 
 def inventory(request):
-    return HttpResponse("Current Inventory View Shows here")
+    return render(request, "inventory.html")
 
 
 def newEntry(request):
-    return HttpResponse("New Entry here")
+    return render(request, "entry.html")
