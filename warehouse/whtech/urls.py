0eb2d877ee613fd1bd6cb92c472e99b4881abfa9
Orens Xhagolli <orens.xhagolli@hotmail.com>
2016-11-12 21:08:27 -0500

    Complete login and logout

--- warehouse/whtech/urls.py
+++ warehouse/whtech/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^login', views.login),
-    url(r'^logout', views.logout),
+    url(r'^login', views.signin, name='signin'),
+    url(r'^logout', views.signout),
     url(r'^inventory', views.inventory),
     url(r'^entry', views.newEntry),
     url(r'^$', views.index, name='index'),
--- warehouse/whtech/views.py
+++ warehouse/whtech/views.py
@@ -1,23 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.urls import reverse
 
 # Create your views here.
 
 
-def login(request):
+def signin(request):
     if request.user.is_authenticated:
         return HttpResponseRedirect(reverse('index'))
-    username = request.POST['username']
-    password = request.POST['password']
-    user = authenticate(username=username, password=password)
-    if user is not None:
-        login(request, user)
-        return HttpResponseRedirect(reverse('index'))
-    else:
-        return render(request, "login.html", {"error": "Could not authenticate!"})
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse('index'))
+        else:
+            return render(request, "login.html", {"error": "Could not authenticate!"})
+    return render(request, "login.html")
 
 
 def signup(request):
@@ -28,13 +30,16 @@ def signup(request):
     return render(request, "signup.html")
 
 
-def logout(request):
-    #logout
-    return render(request, "index.html")
+def signout(request):
+    if request.user.is_authenticated:
+        logout(request)
+    return HttpResponseRedirect(reverse('index'))
 
 
 def index(request):
-    return render(request, "index.html")
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect(reverse('signin'))
+    return render(request, "index.html", {'name': request.user.username})
 
 
 def inventory(request):
