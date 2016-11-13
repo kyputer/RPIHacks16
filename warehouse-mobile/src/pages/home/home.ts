45dbfe4f2f24c9c234a3df5a3a8326f43d3c56a5
Hongbo Miao <Hongbo.Miao@outlook.com>
2016-11-12 23:37:59 -0500

    feat: BarcodeScanner

--- warehouse-mobile/src/pages/home/home.ts
+++ warehouse-mobile/src/pages/home/home.ts
@@ -1,15 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
+import { BarcodeScanner } from 'ionic-native';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
-
+export class HomePage implements OnInit {
   constructor(public navCtrl: NavController) {
 
   }
 
+  ngOnInit() {
+    console.log('ngOnInit');
+
+    BarcodeScanner.scan().then((barcodeData) => {
+      console.log('barcodeData', barcodeData);
+      // Success! Barcode data is here
+    }, (err) => {
+      // An error occurred
+    });
+  }
 }
